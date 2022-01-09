c78afaee97d82e4c2a66f66a3c7f150ea8717f19
Bart Botta <00003b@gmail.com>
2022-01-08 23:04:16 -0600

    fix tests

--- test.lisp
+++ test.lisp
@@ -1,21 +1,27 @@
 (defpackage #:ci-example.test
   (:use #:cl)
   (:local-nicknames (#:x #:ci-example))
-  (:export #:run-tests-for-ci))
+  (:export #:run-tests-for-ci
+           #:run-tests-for-ci2))
 
 (in-package #:ci-example.test)
 
 (defun run-tests-for-ci ()
-  (handler-case
-      (progn
-        ;; add any testing that should run in CI here
-        (assert (= (x:run 1) 2))
-        (unless (= (x:run 2) 3)
-          (format t "test failed~%")
-          (finish-output)
-          (uiop:quit 124)))
-    (error (a)
-      (format t "caught error ~s~%~a~%" a a)
-      (finish-output)
-      (uiop:quit 125))))
+  ;; error on failure
+  (assert (= (x:run 1) 2))
+  (assert (= (x:run 2) 3))
+  ;; if all tests passed, return T
+  t)
+
+(defun run-tests-for-ci2 ()
+  ;; or run multiple tests and report how many passed
+  (let ((ok 0)
+        (failed 0))
+    (flet ((try (x) (if x (incf ok) (incf failed))))
+      (try (= (x:run 3) 4))
+      (try (= (x:run 4) 5))
+      (loop repeat 4 for i from 10 for j from 11 (try (= (x:run i) j)))))
+  (format t "passed ~s, failed ~s~%")
+  ;; return NIL if there were failures, so CI will fail
+  (= failed 0))
 
